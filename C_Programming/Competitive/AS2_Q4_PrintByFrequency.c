4dab8a88efd2169893db018434b85663476a6b21
nilesh-chothave <nileshchothave2003@gmail.com>
2026-06-28 12:32:38 +0530

    Added new program

---
+++ C_Programming/Competitive/AS2_Q4_PrintByFrequency.c
@@ -0,0 +1,50 @@
+/*
+    Q4 Accept two numbers from user and display first number in secod number of times.
+    
+    output:
+        Input : 12 5
+        Output : 12 12 12 12 12
+
+        Input : -2 3
+        Output : -2 -2 -2
+
+        Input : 21 -3
+        Output : 21 21 21
+
+        Input : -2 0
+        Output :
+
+
+*/
+#include<stdio.h>
+
+void Display(int iNo,int iFrequency)
+{
+    int iCnt = 0;
+
+    if(iFrequency < 0)
+    {
+        iFrequency = -iFrequency; // Updater
+    }
+
+    for(iCnt = 1; iCnt <= iFrequency;iCnt++)
+    {
+        printf("%d\t",iNo);
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    int iCount = 0;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    printf("Enter frequency : ");
+    scanf("%d",&iCount);
+
+    Display(iValue,iCount);
+
+    return 0;
+}
