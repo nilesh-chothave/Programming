7f5ee5f0f9b4f2c6a0169d7548fc5a7370b626b3
nilesh-chothave <nileshchothave2003@gmail.com>
2026-06-29 14:31:46 +0530

    Added new program

---
+++ C_Programming/Competitive/AS3_Q2_EvenFactors.c
@@ -0,0 +1,38 @@
+/*
+    2. Write a program which accept number from user and print even factors of that
+    number.
+    
+    Input : 24
+    Output: 1 2 4 6 8 12
+*/
+
+#include<stdio.h>
+
+void DisplayFactor(int iNo)
+{
+    int i = 0;
+
+    if(iNo<=0)
+    {
+        iNo = -iNo;
+    }
+    for(i = 1; i<=(iNo/2);i++)
+    {
+        if((iNo % i)==0 && (i % 2)==0 || i == 1)
+        {
+            printf("%d\t",i);
+        }
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter Number : ");
+    scanf("%d",&iValue);
+
+    DisplayFactor(iValue);
+
+    return 0;
+}
