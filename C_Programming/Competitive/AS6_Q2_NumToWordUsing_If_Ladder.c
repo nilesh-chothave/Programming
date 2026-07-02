cf350d35df76eb50aa8ae2e32ab91d10ce0da516
nilesh-chothave <nileshchothave2003@gmail.com>
2026-07-02 15:08:44 +0530

    origin

---
+++ C_Programming/Competitive/AS6_Q2_NumToWordUsing_If_Ladder.c
@@ -0,0 +1,79 @@
+/*
+    2. Accept single digit number from user and print it into word.
+
+    Input : 9
+    Output : Nine
+
+    Input : -3
+    Output : Three
+    
+    Input : 12
+    Output : Invalid Number
+*/
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    if(iNo == 0)
+    {
+        printf("Zero \n");
+    }
+    else if(iNo == 1)
+    {
+        printf("One \n");
+    }
+    else if(iNo == 2)
+    {
+        printf("Two \n");
+    }
+    else if(iNo == 3)
+    {
+        printf("Three \n");
+    }
+    else if(iNo == 4)
+    {
+        printf("Four \n");
+    }
+    else if(iNo == 5)
+    {
+        printf("Five \n");
+    }
+    else if(iNo == 6)
+    {
+        printf("Six \n");
+    }
+    else if(iNo == 7)
+    {
+        printf("Seven \n");
+    }
+    else if(iNo == 8)
+    {
+        printf("Eight \n");
+    }
+    else if(iNo == 9)
+    {
+        printf("Nine \n");
+    }
+    else
+    {
+        printf("Invalid input \n");
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
