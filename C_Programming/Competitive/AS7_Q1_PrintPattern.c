f06e37da598b920ec7360cec67ee6f7dbc9474f8
nilesh-chothave <nileshchothave2003@gmail.com>
2026-07-02 15:12:06 +0530

    Added new program

---
+++ C_Programming/Competitive/AS7_Q1_PrintPattern.c
@@ -0,0 +1,48 @@
+/*
+    1.Write a program which accept number from user and display below pattern.
+    Input : 5
+    Output : * * * * * # # # # #
+
+    Input : 6
+    Output : * * * * * * # # # # # # #
+
+    Input : -5
+    Output : * * * * * # # # # #
+
+    Input : 2
+    Output : * * # #
+*/
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf(" * ");
+    }
+
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf(" # ");
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
