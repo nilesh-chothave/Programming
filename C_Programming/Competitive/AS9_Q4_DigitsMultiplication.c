ef92668d3989939aee050b1ed8c2f93f869b71e8
nilesh-chothave <nileshchothave2003@gmail.com>
2026-07-04 12:15:48 +0530

    Added new Program

---
+++ C_Programming/Competitive/AS9_Q4_DigitsMultiplication.c
@@ -0,0 +1,50 @@
+/*
+    4.Write a program which accept number from user and return multiplication of all
+    digits.
+
+    Input : 2395
+    Output : 270
+
+    Input : 1018
+    Output : 8
+
+    Input : 9440
+    Output : 144
+    
+    Input : 922432
+    Output : 864
+*/
+
+#include<stdio.h>
+
+int MultDigits(int iNo)
+{
+    int iDigit = 0;
+    int iMult = 1;
+
+    while(iNo != 0)
+    { // 2
+        iDigit = iNo % 10;
+
+        iMult = iMult * iDigit;
+
+        iNo = iNo / 10;
+    }
+
+    return iMult;
+}
+
+int main()
+{
+    int iVlaue = 0;
+    int iRet = 0;
+
+    printf("Enter number : ");
+    scanf("%d",&iVlaue);
+    
+    iRet = MultDigits(iVlaue);
+
+    printf("All Digit multiplication is : %d\n",iRet);
+
+    return 0;
+}
