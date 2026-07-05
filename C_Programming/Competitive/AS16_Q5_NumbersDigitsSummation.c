39a33d1590c20882d8d0ebed63f8d8a280bb1032
nilesh-chothave <nileshchothave2003@gmail.com>
2026-07-05 19:11:37 +0530

    Added new program

---
+++ C_Programming/Competitive/AS16_Q5_NumbersDigitsSummation.c
@@ -0,0 +1,60 @@
+/*
+5. Accept N numbers from user and display summation of digits of each
+number.
+
+Input : N : 6
+Elements :8225 665 3 76 953 858
+Output : 17 17 3 13 17 21
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void DigitSum(int Arr[], int iLength)
+{
+    int iCnt = 0, iDigit = 0, iSum = 0, iTemp = 0;
+
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        iTemp = Arr[iCnt];
+
+       while(iTemp != 0)
+       {
+          iDigit = iTemp % 10;
+          iSum = iSum + iDigit;  
+          iTemp = iTemp / 10;
+       }
+        printf("%d\t",iSum);
+        iSum = 0;
+    }
+}
+
+int main()
+{
+    int iSize = 0, iCnt = 0;
+    int *p = NULL;
+
+    printf("Enter number of elemenets : ");
+    scanf("%d",&iSize);
+
+    p = (int *)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable to allocate memory");
+        return -1;
+    }
+
+    printf("Enter %d elements\n",iSize);
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("Enter element %d: ",iCnt+1);
+        scanf("%d",&p[iCnt]);
+    }
+
+    DigitSum(p, iSize);
+    free(p);
+
+    return 0;
+}
