b0a1ac26270c2020bc78502505c882e0bf1f57d6
nilesh-chothave <nileshchothave2003@gmail.com>
2026-07-08 12:27:38 +0530

    Added new program

---
+++ C_Programming/Competitive/AS24_Q2_LetterLarge&Small.c
@@ -0,0 +1,47 @@
+/*
+    2. Accept number of rows and number of columns from user and display below
+    pattern.
+
+    Input : iRow = 4 iCol = 4
+    Output :A    B   C   D
+            a    b   c   d
+            A    B   C   D
+            a    b   c   d
+*/
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+    char ch = '\0';
+    char ch1 = '\0';
+    
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1, ch = 'A', ch1 = 'a'; j <= iCol; j++, ch++, ch1++)
+        {
+            if(i % 2 != 0)
+            {
+                printf("%c\t",ch);
+            }
+            else if(i % 2 == 0)
+            {
+                printf("%c\t",ch1);
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and columns : ");
+    scanf("%d %d",&iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
