10bc1fb9f417b31313001e597d17b9a2cc6bd71e
nilesh-chothave <nileshchothave2003@gmail.com>
2026-07-08 12:22:28 +0530

    Added new program

---
+++ C_Programming/Competitive/AS22_Q1_alphaPattern.c
@@ -0,0 +1,32 @@
+/*
+    1. Accept number from user and display below pattern.
+    
+    Input : 5
+    Output : A B C D E
+*/
+
+#include<stdio.h>
+
+void Pattern(int iNo)
+{
+    int iCnt = 0;
+    int ch = 65;
+
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("%c\t",ch);
+        ch++;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number of element : ");
+    scanf("%d",&iValue);
+
+    Pattern(iValue);
+
+    return 0;
+}
