a090a70146e2438ac12a093e7e44f4517e512300
nilesh-chothave <nileshchothave2003@gmail.com>
2026-07-10 00:00:24 +0530

    Added new program

---
+++ C_Programming/Competitive/AS29_Q2_CheckCapital.c
@@ -0,0 +1,51 @@
+/*
+    2. Accept Character from user and check whether it is capital or not
+    (A-Z).
+
+    Input : F
+    Output : TRUE
+
+    Input : d
+    Output : FALSE
+*/
+
+#include<stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+BOOL ChkCapital(char ch)
+{
+    if(ch >= 'A' && ch <= 'Z')
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+
+int main()
+{
+    char cValue = '\0';
+    BOOL bRet = FALSE;
+
+    printf("Enter the character  : ");
+    scanf("%c",&cValue);
+
+    bRet = ChkCapital(cValue);
+
+    if(bRet == TRUE)
+    {
+        printf("It is Capitala Character\n");
+    }
+    else
+    {
+        printf("It is not Capital Character\n");
+    }
+
+    return 0;
+}
