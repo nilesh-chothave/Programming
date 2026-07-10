3aa89819303bd8ea4d6cfb8565fccef6edbd49b8
nilesh-chothave <nileshchothave2003@gmail.com>
2026-07-11 01:08:48 +0530

    Added new program

---
+++ C_Programming/Competitive/AS30_Q4_CheckSpecialSymbol.c
@@ -0,0 +1,52 @@
+/*
+    4. Accept Character from user and check whether it is special symbol
+    or not (!, @, #, $, %, ^, &, *).
+
+    Input : %
+    Output : TRUE
+
+    Input : d
+    Output : FALSE
+*/
+
+#include<stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+BOOL ChkSpecial(char ch)
+{
+    if(ch == '!' || ch == '@' || ch == '#' || ch == '$' 
+     ||ch == '%' || ch == '^' || ch == '&' || ch == '*')
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+
+int main()
+{
+    char cValue = '\0';
+    BOOL bRet = FALSE;
+
+    printf("Enter the character : ");
+    scanf("%c",&cValue);
+
+    bRet = ChkSpecial(cValue);
+
+    if(bRet == TRUE)
+    {
+        printf("It is special Character\n");
+    }
+    else
+    {
+        printf("It is a not special Character\n");
+    }
+
+    return 0;
+}
