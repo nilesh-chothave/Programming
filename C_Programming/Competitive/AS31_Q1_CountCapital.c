ed67bec4bc3d8c00dc1f7db0e4914bb4666d0738
nilesh-chothave <nileshchothave2003@gmail.com>
2026-07-11 01:09:23 +0530

    Added new program

---
+++ C_Programming/Competitive/AS31_Q1_CountCapital.c
@@ -0,0 +1,45 @@
+/*
+    1.Write a program which accept string from user and count number of
+    capital characters.
+
+    Input : “Marvellous Multi OS”
+    Output : 4
+*/
+
+#include<stdio.h>
+
+int CountCapital(char *str)
+{
+    int iCount = 0;
+
+    if(str == NULL)
+    {
+        return 0;
+    }
+
+    while(*str != '\0')
+    {
+        if(*str >= 'A' && *str <= 'Z')
+        {
+            iCount++;
+        }
+        str++;
+    }
+    return iCount;
+    
+}
+
+int main()
+{
+    char Arr[20];
+    int iRet = 0;
+
+    printf("Enter string : ");
+    scanf("%[^'\n']s",Arr);
+
+    iRet = CountCapital(Arr);
+
+    printf("%d",iRet);
+
+    return 0;
+}
