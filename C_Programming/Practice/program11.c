285d0276ef394a7463a88e733d62f57fc4027fbe
nilesh-chothave <nileshchothave2003@gmail.com>
2026-07-18 18:19:14 +0530

    C Programming Pratice Files Uploaded.

---
+++ C_Programming/Practice/program11.c
@@ -0,0 +1,44 @@
+/*
+    ALGORITHM
+    
+    START
+        Accept number as No
+        if No is completely divisble by 2
+            then print even
+        otherwise
+             print odd
+    STOP
+    
+    START
+        Accept number as no
+        divide no by 2
+        If remainder is 0
+            then print as even
+        otherwise
+            print as odd
+    STOP
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    int iValue = 0;
+    int iRemainder = 0;
+
+    printf("Enter number : \n");
+    scanf("%d",&iValue);
+
+    iRemainder = iValue % 2;
+
+    if(iRemainder == 0)
+    {
+        printf("Number is Even \n");
+    }
+    else
+    {
+        printf("Number is Odd \n");
+    }
+
+    return 0;
+}
