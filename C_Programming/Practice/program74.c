6c573ed50593a0bead2df9eda66b37fdf3a8f3fb
nilesh-chothave <nileshchothave2003@gmail.com>
2026-08-27 14:42:00 +0530

    C Programming Pratice Files Uploaded.

---
+++ C_Programming/Practice/program74.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int iCnt = 0;
+
+    iCnt = 5;
+
+    while(iCnt>=1)
+    {
+        printf("%d\t",iCnt);
+        iCnt--;
+    }
+
+    printf("\n");
+
+    return 0;
+}
