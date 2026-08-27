ca3f83b71c614f4dbcd218a4731ef59f6cb49f15
nilesh-chothave <nileshchothave2003@gmail.com>
2026-08-27 14:41:08 +0530

    C Programming Pratice Files Uploaded.

---
+++ C_Programming/Practice/program71.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int iCnt = 0;  // loop counter
+
+    iCnt = 1;       // Inslization
+
+    while(iCnt<=5)  // coundition
+    {
+        printf("Jay Ganesh...\n");  // loop body
+        iCnt++;     // Displaysment
+    }
+
+    return 0;
+}
