51fbb5ebfebe711ed5ccb16ed4c9c950dd50bbf1
nilesh-chothave <nileshchothave2003@gmail.com>
2026-09-03 23:49:14 +0530

    C Programming Pratice Files Uploaded.

---
+++ C_Programming/Practice/program106.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void Display(int *iPtr)         // (*) Dereference operator
+{                               // Call By Address
+    printf("%d\n",*iPtr);
+    iPtr++;
+
+    printf("%d\n",*iPtr);
+    iPtr++;
+
+    printf("%d\n",*iPtr);
+}
+
+int main()
+{
+    int Arr [5] = {10,20,30,40,50};
+
+    Display(Arr);
+
+    return 0;
+}
