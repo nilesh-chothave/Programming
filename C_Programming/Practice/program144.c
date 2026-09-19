a50f11aac0a28c00b36e326536d44844c100878d
nilesh-chothave <nileshchothave2003@gmail.com>
2026-09-19 23:53:26 +0530

    C Programming Pratice Files Uploaded

---
+++ C_Programming/Practice/program144.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+void Update(int Arr[], int iSize)   // Call by address
+{
+    int iCnt = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        Arr[iCnt]++;
+    }
+}
+
+int main()
+{
+    int Brr [] = {10,20,30,40,50};  // static Array
+    int iCnt = 0;
+
+    printf("Array elements before function call : \n");
+    for(iCnt = 0; iCnt < 5; iCnt++)
+    {
+        printf("%d\n",Brr[iCnt]);
+    }
+
+    Update(Brr,5);
+
+    printf("Array elements after function call : \n");
+    for(iCnt = 0; iCnt < 5; iCnt++)
+    {
+        printf("%d\n",Brr[iCnt]);
+    }
+
+    return 0;
+}
